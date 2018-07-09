6473891bc69be16d54d929b15a4e9d16bc98f32d
PhilSmith1974 <31818947+PhilSmith1974@users.noreply.github.com>
2018-07-09 17:18:04 +0100

    Add project files.

---
+++ HSH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HSH.Startup))]
+namespace HSH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
