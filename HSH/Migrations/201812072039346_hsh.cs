972a7560cd07a4f76d5fcaaad4063a534a833086
PhilSmith1974 <31818947+PhilSmith1974@users.noreply.github.com>
2018-12-07 23:00:10 +0000

    Fine tuning favourite and Search

---
+++ HSH/Migrations/201812072039346_hsh.cs
@@ -0,0 +1,16 @@
+namespace HSH.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class hsh : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
